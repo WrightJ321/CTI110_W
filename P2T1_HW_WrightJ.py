d1746fbebe205e8ada8142b33a6b8044e95c618a
WrightJ321 <143745753+WrightJ321@users.noreply.github.com>
2023-09-12 14:32:04 -0400

    Add files via upload

---
+++ P2T1_HW_WrightJ.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+
+# Collect the data
+data = [] # empty list
+"""
+print("Enter Pokeman data")
+print("Day 1: ", end="")
+day1 = int(input())
+print("Day 2: ", end="")
+day2 = int(input())
+print("Day 3: ", end="")
+day3 = int(input())
+"""
+# New version
+num_days = int(input("How many days? "))
+for day in range(num_days):
+  print("Day ", day, ":", end="")
+  today = int(input())
+  data.append(today) # add it to the end of the list
+
+
+
+# put the data in a list (done)
+# print min and mX
+print("Best day:", max(data))
+print("Worst day:", min(data))
+
+
+# To do: Graph the real data 
+fig, ax = plt.subplots()
+ax.plot(range(num_days), data)
+plt.title("Pokeman Data")
+plt.xlabel('day #')
+plt.ylabel('pokemans collected')
+plt.show()
